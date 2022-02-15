91b6ea8edef8738728566beb4e87204a57ec25a1
aiminick <aiminick@qq.com>
2022-02-15 22:25:24 +0800

    change docker run script

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run --name webvirtcloud -d -p 80:80 -p 443:443 -p 6080:6080 armv8a/webvirtcloud
+docker run \
+--name webvirtcloud \
+--restart=always \
+-d -p 80:80 \
+-p 443:443 \
+-p 6080:6080 \
+armv8a/webvirtcloud
